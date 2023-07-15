241d3fd7e0a5ebec5632cb3f1b6e8a92ab0fa6fd
jojo228 <49480316+jojo228@users.noreply.github.com>
2023-07-15 01:38:16 +0000

    e

--- TDM/settings.py
+++ TDM/settings.py
@@ -143,11 +143,8 @@ MEDIA_URL = '/images/'
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 CART_SESSION_ID = 'cart'
